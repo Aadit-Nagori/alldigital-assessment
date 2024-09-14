36f22b12b8aef3fcc8560038b5864cc69d1a15e1
Aadit Nagori <51096338+Aadit-Nagori@users.noreply.github.com>
2024-09-14 13:17:19 -0400

    Minor fixes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import joblib
 import logging
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+import cProfile
 
 import secrets
 import passwords as sec
@@ -25,6 +26,11 @@ logging.basicConfig(filename="api_usage.log",level=logging.INFO,format='%(asctim
 
 
 #Defining the input schema
+'''
+This class defines the input schema for the API endpoint.
+The input schema is defined using Pydantic's BaseModel class.
+The input schema includes the optimal fields found during the feature engineering process.
+'''
 class ModelInput(BaseModel):
     SeniorCitizen: bool
     Partner: bool
@@ -37,7 +43,14 @@ class ModelInput(BaseModel):
     MonthlyCharges: float
     TotalCharges: float
 
+        
+        
 #Authentication function
+'''
+This function checks the username and password provided by the user against the correct username and password.
+If the username and password are correct, the function returns the username.
+If the username and password are incorrect, the function raises an HTTPException with status code 401 (Unauthorized).
+'''
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, sec.username)
     correct_password = secrets.compare_digest(credentials.password, sec.password)
@@ -46,6 +59,14 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     return credentials.username
 
 #Defining the API endpoint
+'''
+This endpoint is used to make predictions using the trained model.
+The endpoint takes the input data as a JSON object and the username and password for authentication.
+The endpoint first checks the authentication credentials using the get_current_username function.
+If the authentication is successful, the endpoint processes the input data, makes a prediction using the model, and returns the prediction.
+If there is an error during the prediction process, the endpoint logs the error and returns an HTTP 500 error.
+If the payment method provided in the input data is invalid, the endpoint raises an HTTP 400 error.
+'''
 @app.post("/predict")
 def predict(data: ModelInput, username: str = Depends(get_current_username)):
     try:
@@ -65,7 +86,6 @@ def predict(data: ModelInput, username: str = Depends(get_current_username)):
             raise HTTPException(status_code=400, detail="Invalid PaymentMethod")
         
         # Convert the input into the format expected by the model
-   
         data = [[
                 int(data.SeniorCitizen),
                 int(data.Partner),
@@ -74,7 +94,7 @@ def predict(data: ModelInput, username: str = Depends(get_current_username)):
                 int(data.OnlineBackup),
                 int(data.TechSupport),
                 int(data.StreamingTV),
-                data.PaymentMethod,  # Assume the model expects encoded categories for PaymentMethod
+                data.PaymentMethod,  
                 data.MonthlyCharges,
                 data.TotalCharges
             ]]
@@ -85,8 +105,8 @@ def predict(data: ModelInput, username: str = Depends(get_current_username)):
         # Return prediction result
         return {"prediction": int(prediction[0])}
     
+    # Re-raise HTTPExceptions so they are returned as-is
     except HTTPException as http_exc:
-        # Re-raise HTTPExceptions so they are returned as-is
         raise http_exc
     
     except Exception as e:
